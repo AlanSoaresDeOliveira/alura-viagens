ef8eb69bedc806c41b08d930482c6dabd6b22f81
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-11-24 14:14:05 -0300

    Initial Commit

---
+++ AluraViagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AluraViagens
+//
+//  Created by Alan Soares de Oliveira on 24/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
