be4b81102abe94d89dcbea4169b3a360e01357d0
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-11-26 11:08:58 -0300

    Create application

--- AluraViagens/ViewController.swift
+++ AluraViagens/ViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+      
+    @IBOutlet weak var tabelaViagem: UITableView!
+    
+    let listaDeViagens: [String] = ["Rio de Janeiro", "Ceará", "São Paulo"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tabelaViagem.dataSource = self
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaDeViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        celula.textLabel?.text = listaDeViagens[indexPath.row]
+        
+        return celula
     }
 
 
